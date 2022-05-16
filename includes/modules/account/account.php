05ef38a83067dc627584d94f995274ed8b35f993
Bjorn Harvold <bjorn@harvold.com>
2022-05-16 12:39:00 +0700

    build: :bulb: initial commit

---
+++ includes/modules/account/account.php
@@ -0,0 +1,42 @@
+<?php
+
+class IKTDAccount extends ET_Builder_Module {
+	protected $namespace = 'iko-travel';
+	public $slug       = 'iktd_account';
+	public $vb_support = 'on';
+
+	protected $module_credits = array(
+		'module_uri' => 'https://iko.travel/',
+		'author'     => 'iko.travel',
+		'author_uri' => 'https://iko.travel/',
+	);
+
+	public function init() {
+		$this->name = esc_html__( 'iko Account', $this->namespace );
+		$this->settings_modal_toggles  = array(
+			'iko' => array(
+				'toggles' => array(
+					'ikoOptions'   => esc_html( 'iko Settings', $this->namespace )
+				),
+			),
+		);
+	}
+
+	public function get_fields() {
+		return array(
+			'content' => array(
+				'label'           => __( "This component does not require any configuration.", $this->namespace ),
+				'type'            => 'iktd_input',
+				'option_category' => 'basic_option',
+				'description'     => __( "Simply ensure that you have entered the correct Client-ID and Client-Secret in the iko plugin settings.", $this->namespace ),
+				'toggle_slug'     => 'ikoOptions',
+			),
+		);
+	}
+
+	public function render( $attrs, $content = null, $render_slug ) {
+		return do_shortcode('[ikoaccount]');
+	}
+}
+
+new IKTDAccount;
